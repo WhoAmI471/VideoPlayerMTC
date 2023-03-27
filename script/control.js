961435f14105be7eb72113c27896ea6677bf28a0
WhoAmI471 <artglad05@mail.ru>
2023-03-27 18:05:00 +0500

    add one player, two flows

--- script/control.js
+++ script/control.js
@@ -8,33 +8,68 @@ const root = document.querySelector(':root');
 
 
 var video = document.getElementById('myVideo');
+var video2 = document.getElementById('myVideo_flow2');
 var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
-var timebar = document.getElementById('timebar');
 
-// video = document.querySelector('#video');
-// progress = document.querySelector('#progress');
+// содержат в себе время, в секундах первого потока и второго
+var progress1 = 0; // первый
+var progress2 = 0; // второй
+
+// флаг для проверки, Был запущен плеер с начала или нет
+var isPlay = false;
+
+progress = document.querySelector('#progress');
 
 video.ontimeupdate = progressUpdate;
+video2.ontimeupdate = progressUpdate2;
 progress.onclick = videoRewind;
 
+// флаг, чтобы функция не срабатывала много раз за секунду
+let flashHandled = false; 
 
-video.addEventListener('timeupdate', function() {
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+// проверяет второй поток, и накладывает фильтр на первый, 
+// если значение яркости превысило допустимое.
+video2.addEventListener('timeupdate', function() {
+  context.drawImage(video2, 0, 0, canvas.width, canvas.height);
 
   var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
   var brightness = calculateBrightness(imageData);
 
-  if (brightness > 150) {
-    handleFlash();
+  if (brightness > 150 && !flashHandled) {
+    setTimeout(function() { // задержка срабатывания фильтра для основного потока
+      handleFlash();
+      flashHandled = true;
+    }, 
+      1000 // 1с. потому что основной видео поток запускается на секунду позже
+    );
   } else {
-    setTimeout(DelitBlur,15000)
+    setTimeout(function() {
+      DelitBlur();
+      flashHandled = false
+    }, 
+      15000 // 15с.
+    );
   }
 });
 
 
 function play() {
-  video.play();
+  // если плеер был запущен с самого начала, то первый поток запускаем с задержкой в с.
+  if (!isPlay) {
+    video2.play();
+    video2.volume = 0;
+    setTimeout(function() {
+      video.play()
+    }, 
+    1000
+    );
+    isPlay = true;
+  } else { // иначе, запускаем два потока одновременно
+    video.play()
+    video2.play();
+  }
 
   root.style.setProperty("--pl", `${0}px`);
   root.style.setProperty("--pa", `${30}px`);
@@ -43,6 +78,8 @@ function play() {
 
 function pause() {
   video.pause();
+  video2.pause();
+
   root.style.setProperty("--pa", `${0}px`);
   root.style.setProperty("--pl", `${30}px`);
 
@@ -56,11 +93,38 @@ function videoVolume() {
   video.volume = v / 100;
 }
 
-function progressUpdate(){
+function progressUpdate(){ // для первого потока
   let dur = video.duration;
   let cur = video.currentTime;
 
   progress.value = (100 * cur) / dur;
+  
+  progress1 = progress.value;
+
+  console.log("1: " + progress.value);
+
+  var hours = Math.floor(progress1 / 60 / 60);
+  var minutes = Math.floor(progress1 / 60) - (hours * 60);
+  var seconds = (progress1 % 60).toFixed(0);
+  
+  
+  var formatted = hours + ':' + minutes + ':' + seconds;
+
+  document.querySelector('#timeout').innerHTML = `${formatted}`;
+}
+
+function progressUpdate2(){ // для второго потока
+  progress2 = (100 * video2.currentTime) / video2.duration;
+  
+  console.log("2: " + progress2);
+  
+  var hours2 = Math.floor(progress2 / 60 / 60);
+  var minutes2 = Math.floor(progress2 / 60) - (hours2 * 60);
+  var seconds2 = (progress2 % 60).toFixed(0);
+  
+  var formatted2 = hours2 + ':' + minutes2 + ':' + seconds2;
+
+  document.querySelector('#timeout2').innerHTML = `${formatted2}`;
 }
 
 function videoRewind() {
@@ -71,7 +135,8 @@ function videoRewind() {
 
   // video.pause();
   video.currentTime = video.duration * (o/w);
-  // video.play();
+  video2.currentTime = video.duration * (o/w) + 1;
+  // video2.play();
 }
 
 function calculateBrightness(imageData) {
@@ -89,8 +154,10 @@ function handleFlash() {
   // Действия при обнаружении яркой вспышки света
   console.log("ОПАСНО")
   
-  var videoWrapper = document.querySelector('.video');
-  videoWrapper.classList.add('contrast');
+  if(!flashHandled){
+    var videoWrapper = document.querySelector('.video');
+    videoWrapper.classList.add('contrast');
+  }
 }
 
 function DelitBlur() {
